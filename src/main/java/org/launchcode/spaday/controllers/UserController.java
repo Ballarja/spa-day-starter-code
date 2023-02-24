05d3d14846607c7c56832d11293fa9b8ba01a771
Ballarja <JamesBallard87@hotmail.com>
2023-02-23 20:33:12 -0600

    code added for studio 12

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,19 +3,23 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("user", new Users());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute Users user, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid Users user, Errors errors, String verify) {
         model.addAttribute("user", user);
         model.addAttribute("verify", verify);
         model.addAttribute("username", user.getUsername());
--- src/main/java/org/launchcode/spaday/models/Users.java
+++ src/main/java/org/launchcode/spaday/models/Users.java
@@ -1,8 +1,20 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Users {
+
+    @NotBlank(message = "Field is Empty")
+    @Size(min = 5, max = 15, message = "Must be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Invalid email format.")
     private String email;
+
+    @Min(value = 6, message = "Must be 6 characters long")
     private String password;
 
     public Users() {
